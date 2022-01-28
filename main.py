b7583c588d6c1e55e15627a8547e57e95ef8eead
Robin Joseph <rjrdutg@gmail.com>
2022-01-28 16:15:14 +0100

    making get_value function wich allow to get the value of a gave cell, gaving also the file path and the sheet, knowing that there is a default value for the sheet name.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pandas
+import openpyxl
 
 files = [
     "excel/test1.xlsx",
     "excel/test2.xlsx"
 ]
+
+def get_value(file, cell_position,sheet='Feuil1'):
+    "This function is getting and returning the value of the gave cell, in a sheet of a file gave too"
+    workbook = openpyxl.load_workbook(file)
+    sheet = workbook[sheet]
+    value = sheet[cell_position].value
+    return value
+
 
 def combining_files(files):
     """
